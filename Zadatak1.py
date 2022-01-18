b4664c4761c996ab6346cabfcfb21781425c6064
Aleksandar N <97960212+Minozen@users.noreply.github.com>
2022-01-19 00:17:15 +0100

    Add files via upload

---
+++ Zadatak1.py
@@ -0,0 +1,4 @@
+# Ovo je strasno jednostavan program, samo ukucamo opseg brojeva i ponovimo to 100 puta u for petlji
+for n in range(1, 101):
+    print(n, end=' Aleksandar\n')
+# Taj \n na kraju mojeg imena sluzi Python-u da zna da pocne sledeci red kad odstampa rezultat 
