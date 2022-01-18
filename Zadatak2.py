4571ab04f9e7d97f48afbf47334e02dac59f6e7b
Minozen <97960212+Minozen@users.noreply.github.com>
2022-01-18 16:30:23 +0100

    Add files via upload

---
+++ Zadatak2.py
@@ -0,0 +1,9 @@
+# Pocnemo sa definisanjem 2 variable-a koji simbolisu dva dela matematickog izraza
+a = 512-282
+b = 47*48+5
+
+# Nasa kalkulacija je jednostavna, podeliti variable a sa variable b
+izraz = 'a/b'
+
+# Stampaj izraz 
+print(eval(izraz))
