bdb4d856d4bcf39f02749f0978236af923aaf722
Aleksandar N <97960212+Minozen@users.noreply.github.com>
2022-01-19 00:27:24 +0100

    Add files via upload

---
+++ For petlja/Zadatak3.py
@@ -0,0 +1,3 @@
+# Jedina stvar koju hocu da obelezim ovde je ta treca cifra u opsegu (range), ona sluzi kao instrukcije Python-u kako da prodje kroz opseg dok prva 2 broja sluze kao pocetak i kraj
+for n in range(100, 2, -2):
+    print(n)
