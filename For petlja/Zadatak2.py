180a5108a5e14666457ead40a678cd3756bc742c
Aleksandar N <97960212+Minozen@users.noreply.github.com>
2022-01-19 00:23:41 +0100

    Add files via upload

---
+++ For petlja/Zadatak2.py
@@ -0,0 +1,4 @@
+
+# Ovo je isto veoma jednostavan program, samo ukucamo opet opseg brojeva i ponovimo to 20 puta u for petlji *sa* operatorom eksponenta, to su te dve zvezdice na liniji 4
+for n in range(1, 21):
+    print(n,'---', n ** 2, end='\n')
