c5b867d92711cde8cc9e262f0c1c595153ad1381
Aleksandar N <97960212+Minozen@users.noreply.github.com>
2022-01-20 05:38:46 +0100

    Add files via upload

---
+++ Brojevi/Zadatak3.py
@@ -0,0 +1,8 @@
+from random import randint
+
+x = randint(1, 10)
+
+for n in range(1, x):
+    print(n, end=' Aleksandar\n')
+
+# Sve funkcije koje smo koristili ovde sam vec objasnio i pricao u proslim zadacima, pa ne vidim da je potrebno da sve ponovimo opet, *veoma* je jednostavan program
