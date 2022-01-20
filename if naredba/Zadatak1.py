6be2953f5a1704d97ae52b522c350baf8eb085ff
Aleksandar N <97960212+Minozen@users.noreply.github.com>
2022-01-20 08:58:32 +0100

    Update Zadatak1.py

--- if naredba/Zadatak1.py
+++ if naredba/Zadatak1.py
@@ -6,7 +6,7 @@ cm = float(input("Ukucaj duzinu u cm:"))
 
 inch = cm/2.54 # Pravimo jos jedan variable koji je je taj float unosen od strane korisnika podeljeno sa 2.54 da dobijemo formulu za ince
 
-if cm <= 0: # Proveravamo da li je broj jednak ili manji nuli
+if cm <= 0: # Proveravamo da li je broj jednak ili manji od nule
    print("Ubacili ste negativan broj ili nulu, to je neispravna vrednost.")
 else:
     print("Duzina u incima:", inch,"in") # Kalkulisemo i stampamo vrednost
