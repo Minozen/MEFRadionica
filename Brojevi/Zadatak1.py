4f4b3b0ec8e962ef164338b60c272c29f7301fb3
Aleksandar N <97960212+Minozen@users.noreply.github.com>
2022-01-20 05:29:23 +0100

    Update Zadatak1.py

--- Brojevi/Zadatak1.py
+++ Brojevi/Zadatak1.py
@@ -2,10 +2,7 @@
 # Zasto nismo samo uradili "from random import randint"? Zato sto funkcija "randint" radi samo sa *integer*, a ne sa float, koristim float zato sto nam daje vecu varijaciju u broju nego da smo koristili samo cele brojeve (integer)
 import random
 
-
 brojevi=[]
-
-# Pravimo variable "br1" koji se sastoji od slucajnih float-a od 1 do 50
 
 for n in range(1, 51): # Pravimo for petlju koja generise pseudo-slucajni broj (float) 50 puta i stavlja ga u listu "brojevi"
     br = random.uniform(3,6)
